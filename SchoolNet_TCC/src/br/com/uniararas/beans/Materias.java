d484008b67854e5d6058ff765dbba4a73879819c
gdonscoi <donscoi@hotmail.com>
2013-08-10 03:40:38 -0300

    add classe

---
+++ SchoolNet_TCC/src/br/com/uniararas/beans/Materias.java
@@ -0,0 +1,75 @@
+package br.com.uniararas.beans;
+
+public class Materias {
+
+	private String nomemateria;
+	
+	private String primeiranota;
+	
+	private String notaspa;
+	
+	private String segundanota;
+	
+	private String mediafinal;
+
+	private String numerofaltas;
+	
+	private String numerofaltaslimite;
+
+	public String getNomemateria() {
+		return nomemateria;
+	}
+
+	public void setNomemateria(String nomemateria) {
+		this.nomemateria = nomemateria;
+	}
+
+	public String getPrimeiranota() {
+		return primeiranota;
+	}
+
+	public void setPrimeiranota(String primeiranota) {
+		this.primeiranota = primeiranota;
+	}
+
+	public String getNotaspa() {
+		return notaspa;
+	}
+
+	public void setNotaspa(String notaspa) {
+		this.notaspa = notaspa;
+	}
+
+	public String getSegundanota() {
+		return segundanota;
+	}
+
+	public void setSegundanota(String segundanota) {
+		this.segundanota = segundanota;
+	}
+
+	public String getMediafinal() {
+		return mediafinal;
+	}
+
+	public void setMediafinal(String mediafinal) {
+		this.mediafinal = mediafinal;
+	}
+
+	public String getNumerofaltas() {
+		return numerofaltas;
+	}
+
+	public void setNumerofaltas(String numerofaltas) {
+		this.numerofaltas = numerofaltas;
+	}
+
+	public String getNumerofaltaslimite() {
+		return numerofaltaslimite;
+	}
+
+	public void setNumerofaltaslimite(String numerofaltaslimite) {
+		this.numerofaltaslimite = numerofaltaslimite;
+	}
+	
+}
