187e0e7f8ae820cc7835b0a92d4dbffb71c48e83
gdonscoi <donscoi@hotmail.com>
2013-09-26 23:38:46 -0300

    PARSE JSON sem GSON

--- SchoolNet_TCC/src/br/com/uniararas/beans/AnoSemestre.java
+++ SchoolNet_TCC/src/br/com/uniararas/beans/AnoSemestre.java
@@ -1,13 +1,9 @@
 package br.com.uniararas.beans;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+
 
 public class AnoSemestre {
 
-	public List<Map<String, String>> files = new ArrayList<Map<String, String>>(4);
-	 
 	public String anolevito;
 	
 	public String semestre;
--- SchoolNet_TCC/src/br/com/uniararas/services/ConsultaAnoSemestreService.java
+++ SchoolNet_TCC/src/br/com/uniararas/services/ConsultaAnoSemestreService.java
@@ -2,21 +2,18 @@ package br.com.uniararas.services;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
-import br.com.uniararas.beans.AnoSemestre;
 import br.com.uniararas.resources.WebServiceCall;
 import br.com.uniararas.util.Constantes;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class ConsultaAnoSemestreService {
 	
 	public ArrayList<HashMap<String,String>> obterAnoSemestre() throws Exception {
-		ArrayList<HashMap<String,String>> listaAnoSemestre = null;
+		ArrayList<HashMap<String,String>> listaAnoSemestre = new ArrayList<HashMap<String,String>>();
 		try {
 			WebServiceCall webServiceCall = WebServiceCall.getInstance();
 			String[] resposta = webServiceCall.get("","", Constantes.URL_OBTER_ANO_SEMESTRE);
@@ -24,18 +21,15 @@ public class ConsultaAnoSemestreService {
 			if(!resposta[0].equals("200"))
 				throw new Exception("Erro ao obter faltas.");
 			
-			java.lang.reflect.Type listType = new TypeToken<List<AnoSemestre>>() {}.getType();
-			List<AnoSemestre> anoSemestre = new Gson().fromJson( resposta[1].trim() , listType);
+			JSONObject mainObject = new JSONObject(resposta[1].trim());
 			
-			listaAnoSemestre = new ArrayList<HashMap<String,String>>();
-			
-			for (AnoSemestre anosemestre : anoSemestre){
-				
+			Iterator<String> keys = mainObject.keys();
+			while(keys.hasNext()){
+				String key = keys.next();
+				JSONObject value = mainObject.getJSONObject(key);
 				HashMap<String, String> map = new HashMap<String, String>();
-				
-				map.put(Constantes.TAG_ANO, anosemestre.anolevito);
-				map.put(Constantes.TAG_SEMESTRE, anosemestre.semestre);
-
+				map.put(Constantes.TAG_ANO, value.getString("anolevito"));
+				map.put(Constantes.TAG_SEMESTRE, value.getString("semestre"));
 				listaAnoSemestre.add(map);
 			}
 			
